8cce8972fe7aebc8225490579a05265960c00f91
Chris Freitas <chrisjf777@gmail.com>
2020-07-20 21:21:36 -0400

    healthscript
    implemented basic health script

---
+++ Super Elmo World/Assets/Code/HealthScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int maxHealth = 3;
+    int currentHealth = 1;
+    public void addHealth(int amt)
+    {
+        //picking up power up
+        if(currentHealth < maxHealth){
+        currentHealth = currentHealth + amt;
+            //checks that it doesnt go above max
+            if(currentHealth > maxHealth)
+            {
+             currentHealth = maxHealth;
+            }
+        }
+        //if already maxed
+        else{
+            Debug.Log("Health already max");
+        }
+    }
+
+    public void subHealth(int amt)
+        {
+            //taking damage
+            currentHealth = currentHealth - amt;
+        }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
