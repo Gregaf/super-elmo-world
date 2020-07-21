9e051d52d29104cabeaa7fe82fd9a16fb431db4f
Chris Freitas <chrisjf777@gmail.com>
2020-07-21 16:36:58 -0400

    Blocks
    Implemented block and destructible block

---
+++ Super Elmo World/Assets/Code/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Block : MonoBehaviour
+{
+    public GameObject contains;
+    public LayerMask detectMask;
+    public float rayLength = 0.6f;
+    public bool activated = false;
+    public virtual void OnHitBlock()
+    {
+        activated = true;
+    }
+    public void Update()
+   {
+       Debug.DrawRay(this.transform.position,Vector2.down * rayLength, Color.green);
+      if( Physics2D.Raycast(this.transform.position, Vector2.down,rayLength, detectMask) && !activated)
+      {
+          OnHitBlock();
+      }
+       
+   }
+}
