1980a945cdbdda1eb0cfa43bf7a97473867a298c
Gregory Freitas <gregfreitas1997@gmail.com>
2020-07-21 17:24:04 -0400

    Added Main Menu
    Very basic layout for the main menu

---
+++ Super Elmo World/Assets/Code/MainMenuManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Canvas[] screens;
+
+    private Canvas currentScreen;
+    private Canvas previousScreen;
+
+    private void Start()
+    {
+        currentScreen = screens[0];
+
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Start the game.");
+    }
+    public void GoToOptions()
+    {
+        Debug.Log("Selected Options.");
+        currentScreen.gameObject.SetActive(false);
+
+        previousScreen = currentScreen;
+
+        currentScreen = screens[1];
+
+        currentScreen.gameObject.SetActive(true);
+    }
+
+    public void ReturnToPrevious()
+    {
+        Canvas temp = currentScreen;
+
+        temp.gameObject.SetActive(false);
+
+        currentScreen = previousScreen;
+        previousScreen = temp;
+
+        currentScreen.gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game.");
+    }
+
+
+
+}
